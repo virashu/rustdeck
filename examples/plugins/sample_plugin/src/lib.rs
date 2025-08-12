b6bf21157447955c6d66e6336569c9e32ed19b38
virashu <virashu@disroot.org>
2025-08-12 21:14:23 +0300

    refactor(examples): remove deprecated macro plugin declaration

--- examples/plugins/sample_plugin/src/lib.rs
+++ examples/plugins/sample_plugin/src/lib.rs
@@ -1,7 +1,12 @@
 #![allow(clippy::unnecessary_wraps)]
 #![allow(unsafe_op_in_unsafe_fn)]
 
-use rustdeck_common::{Args, export_plugin};
+use rustdeck_common::{
+    Args, Type,
+    builder::{Action, PluginBuilder, Variable},
+    decorate_fn_get_variable, decorate_fn_init, decorate_fn_run_action, decorate_fn_update,
+    export_plugin,
+};
 
 struct PluginState {
     counter: i32,
@@ -42,69 +47,6 @@ fn run_action(
 
     Ok(())
 }
-
-/*
- * Definition using macros
- */
-
-// use rustdeck_common::{
-//     actions, args, decl_action, decl_arg, decl_plugin, decl_variable, variables,
-// };
-//
-// export_plugin! {
-//     decl_plugin! {
-//         id: "plugin_test",
-//         name: "Sample Plugin",
-//         desc: "A sample plugin",
-//         variables: variables!(
-//             decl_variable! {
-//                 id: "counter",
-//                 desc: "Counter",
-//                 vtype: "string",
-//             },
-//         ),
-//         actions: actions!(
-//             decl_action! {
-//                 id: "increment",
-//                 name: "Increment",
-//                 desc: "Increment counter",
-//             },
-//             decl_action! {
-//                 id: "add",
-//                 name: "Add",
-//                 desc: "Add value to counter",
-//                 args: args!(
-//                     decl_arg! {
-//                         id: "amount",
-//                         name: "Amount",
-//                         desc: "Amount",
-//                         vtype: "int",
-//                     },
-//                 ),
-//             },
-//             decl_action! {
-//                 id: "clear",
-//                 name: "Clear",
-//                 desc: "Set counter value to 0",
-//             },
-//         ),
-
-//         fn_init: init,
-//         fn_update: update,
-//         fn_get_variable: get_variable,
-//         fn_run_action: run_action,
-//     }
-// }
-
-/*
- * Definition using builder pattern
- */
-
-use rustdeck_common::{
-    Type,
-    builder::{Action, PluginBuilder, Variable},
-    decorate_fn_get_variable, decorate_fn_init, decorate_fn_run_action, decorate_fn_update,
-};
 
 export_plugin! {
     PluginBuilder::new("plugin_test", "Sample Plugin", "A sample plugin")
